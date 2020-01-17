9d2b10b4b89e6b9521398ccc99e80a9d73fbbaed
ali225 <aligamal_25@yahoo.com>
2020-01-17 17:15:44 +0200

    LCD_main.c

--- LCD_main.c
+++ LCD_main.c
@@ -4,23 +4,27 @@
  *  Created on: Jan 2, 2020
  *      Author: Eng Ali Gamal
  */
-/*#include <avr/io.h>
+/*
+
+#include <avr/io.h>
 #include "util/delay.h"
 #include "util/delay.h"
 #include "LIB/std_types.h"
 #include "HAL/LCD/inc/HAL_lcd.h"
 
- main() {
+ int main() {
 
 	stdReturnType_t status;
-
+while(1){
 	char st[] = {'E', 'N','g','/' ,'A', 'i', 'L', '\0' };
 	char arr[] = {"Eng Ali gamal aziz "};
 	status = LCD_init();
-	status |= LCD_displayString_rowCol(arr,1,3);
-	status |=LCD_clearScreen();
-	status |=LCD_displayString_rowCol(st,0,2);
+	status |= LCD_displayString_rowCol(arr,0,2);
+	//status |=LCD_clearScreen();
+	status |=LCD_displayString_rowCol(st,1,2);
 	_delay_ms(1000);
-	status |=LCD_clearScreen();
-	return 0;
-}*/
+	//status |=LCD_clearScreen();
+}
+	return status;
+}
+*/
